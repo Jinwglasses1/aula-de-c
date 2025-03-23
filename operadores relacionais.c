8dde79b241bd537b924fe540cd35026e157f5dd6
Jinwglasses1 <202503460931@alunos.estacio.br>
2025-03-23 13:48:29 +0000

    atualizado

---
+++ operadores relacionais.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    float numero1 = 10.2;
+    int numero2 = 5;
+    float numero3 = 2.1;
+    int numero4 = 2;
+
+    printf("10.2 é maior que 5?: %d \n",  numero1 >=  numero2);
+    printf("10.2 é igual a 5?: %d \n",  numero1 ==  numero2);
+    printf("2.1 é maior que 2?: %d \n", numero3 > numero4);
+    printf("2.1 é igual a 2?: %d \n", numero3 == numero4);
+
+    return 0;
+
+}
---
+++ unsigned,signed e etc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int numerocomsinal = 3000000000; // existe um limite para número negativo e positivo
+    unsigned int numerosemsinal = 3000000000; 
+
+    printf("Número: %d \n", numerocomsinal);
+    printf("Número: %u \n", numerosemsinal);
+
+
+
+
+    return 0;
+
+}
