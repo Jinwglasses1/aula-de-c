7c6762a9e98ffeb171ba9c915f060e24b80d2151
Jinwglasses1 <202503460931@alunos.estacio.br>
2025-03-25 20:45:54 +0000

    finalizado

---
+++ utilizando o operador %.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+    int resultado;
+    
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    resultado =  numero % 2;
+
+    printf("O resto da divisão é igual a: %d", resultado);
+
+    if (resultado == 0) //nunca pôr ponto e vírgula no final do if.
+    {
+        printf("\nO número é par!\n");
+    }
+
+    return 0;
+
+}
