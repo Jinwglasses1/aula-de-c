4ddf45ab5f3e260da751517c2268f814a7dee7a8
Jinwglasses1 <202503460931@alunos.estacio.br>
2025-03-25 20:30:43 +0000

    feito

--- estrutura de decisões simples.c
+++ estrutura de decisões simples.c
@@ -2,10 +2,20 @@
 
 int main(){
 
+    int numero1, numero2;
+    
+    printf("Digite o primeiro número: ");
+    scanf("%d", &numero1);
 
+    printf("Digite o segundo número: ");
+    scanf("%d", &numero2);
 
-
-
-
+    if (numero1 > numero2) // se não for maior, o comando print não será executado.
+    {
+        printf("O primeiro número é maior que o segundo! \n");
+    
+    }
+    
+    return 0;
 
 }
